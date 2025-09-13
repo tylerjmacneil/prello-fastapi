6b1a2ac39a6192c072e3e5271fc0327eac92902b
Tyler <tylermacneil2002@gmail.com>
2025-09-12 22:42:11 -0400

    register clients router

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from app.routers import health, clients
 
 app = FastAPI()
 
-app.include_router(health.router, prefix="/health", tags=["health"])
-app.include_router(clients.router, prefix="/clients", tags=["clients"])
-
+# Root
 @app.get("/")
 def read_root():
     return {"hello": "prello"}
+
+# Routers
+app.include_router(health.router, prefix="/health", tags=["health"])
+app.include_router(clients.router, prefix="/clients", tags=["clients"])
 
